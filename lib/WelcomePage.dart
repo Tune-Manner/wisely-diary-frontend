4d7b95d4a8d32ccfb2a3776df6d3a26e8b204418
eurdream98 <k01054792361@gmail.com>
2024-07-26 16:52:33 +0900

    feat : google logout

--- lib/WelcomePage.dart
+++ lib/WelcomePage.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'main.dart';
 class WelcomePage extends StatelessWidget {
   final String? userId;
 
   const WelcomePage({super.key, required this.userId});
 
+  Future<void> _signOut(BuildContext context) async {
+    await Supabase.instance.client.auth.signOut();
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => MainApp()),
+          (Route<dynamic> route) => false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Welcome')),
+      appBar: AppBar(
+        title: Text('Welcome'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => _signOut(context),
+          ),
+        ],
+      ),
       body: Center(
         child: Text('${userId ?? 'Guest'}님 환영합니다!'),
       ),
