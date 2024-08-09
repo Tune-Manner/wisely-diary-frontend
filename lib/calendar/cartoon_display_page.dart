05f54b0ed310c0de77645ba8d9f75c68b08b65a0
eurdream98 <k01054792361@gmail.com>
2024-08-09 12:09:30 +0900

    feat: select date and auth and find cartoon

--- lib/calendar/cartoon_display_page.dart
+++ lib/calendar/cartoon_display_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
 
 class CartoonDisplayPage extends StatelessWidget {
   final List<dynamic> cartoons;
 
   CartoonDisplayPage({required this.cartoons});
+
+  Widget _buildImage(String imagePath) {
+    final filename = path.basename(imagePath); // 파일 이름만 추출
+    final imageUrl = 'http://10.0.2.2:8080/cartoonImage/$filename';
+
+    return Image.network(
+      imageUrl,
+      errorBuilder: (context, error, stackTrace) {
+        print('Error loading network image: $error');
+        return Text('이미지를 불러올 수 없습니다: $imageUrl');
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +29,12 @@ class CartoonDisplayPage extends StatelessWidget {
         itemCount: cartoons.length,
         itemBuilder: (context, index) {
           final cartoon = cartoons[index];
+          final imagePath = cartoon['cartoonPath'];
+
           return Card(
             child: Column(
               children: [
-                Image.network(
-                  'http://10.0.2.2:8080/cartoonImage/${cartoon['cartoonPath'].split('\\').last}',
-                  errorBuilder: (context, error, stackTrace) {
-                    return Text('이미지를 불러올 수 없습니다.');
-                  },
-                ),
+                _buildImage(imagePath),
                 Text('생성 시간: ${cartoon['createdAt']}'),
               ],
             ),
