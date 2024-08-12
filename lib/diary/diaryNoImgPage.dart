96f67afb0f4f8d5119adbdfe59c0abd166a7ad72
djfksdl <djfksdl@naver.com>
2024-08-12 16:42:59 +0900

    feat: 일기 조회 페이지 구현 완료

--- lib/diary/diaryNoImgPage.dart
+++ lib/diary/diaryNoImgPage.dart
@@ -6,23 +6,88 @@ class DiaryNoImgPage extends StatefulWidget {
 }
 
 class _DiaryNoImgPageState extends State<DiaryNoImgPage> {
-  bool _isGiftMenuVisible = false;
+  OverlayEntry? _overlayEntry;
+  bool _isOverlayVisible = false;  // 추가: 오버레이 가시성 상태를 관리하는 변수
 
   void _toggleGiftMenu() {
     setState(() {
-      _isGiftMenuVisible = !_isGiftMenuVisible;
+      if (!_isOverlayVisible) {
+        _overlayEntry = _createOverlayEntry();
+        Overlay.of(context)!.insert(_overlayEntry!);
+        _isOverlayVisible = true;
+      } else {
+        _overlayEntry?.remove();
+        _overlayEntry = null;
+        _isOverlayVisible = false;
+      }
     });
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    return OverlayEntry(
+      builder: (context) => Positioned(
+        bottom: 90,
+        right: 25,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            width: 75,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Column(
+                  children: [
+                    Image.asset('assets/music_icon.png'),
+                    SizedBox(height: 4),
+                    Text('맞춤노래', style: TextStyle(fontSize: 10)),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  children: [
+                    Image.asset('assets/cuttoon_icon.png'),
+                    SizedBox(height: 4),
+                    Text('네컷만화', style: TextStyle(fontSize: 10)),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Column(
+                  children: [
+                    Image.asset('assets/letter_icon.png'),
+                    SizedBox(height: 4),
+                    Text('위로의 편지', style: TextStyle(fontSize: 10)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFFFF9F2), // 상단 배경 색상
+        backgroundColor: Color(0xFFFFF9F2),
         elevation: 0,
-        leadingWidth: 100, // 두 개의 아이콘이 충분히 들어갈 공간을 확보하기 위해 너비 설정
+        leadingWidth: 100,
         leading: Row(
-          mainAxisSize: MainAxisSize.min, // Row의 크기를 최소화하여 아이콘들이 나란히 배치되도록 함
+          mainAxisSize: MainAxisSize.min,
           children: [
             IconButton(
               icon: Icon(Icons.arrow_back, color: Colors.black),
@@ -39,93 +104,92 @@ class _DiaryNoImgPageState extends State<DiaryNoImgPage> {
           ],
         ),
       ),
-      body: Container(
-        color: Color(0xFFFFF9F2), // 전체 배경 색상
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white, // 배경 흰색
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          Container(
+            color: Color(0xFFFFF9F2),
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Flexible(
-                          child: Text(
-                            '2024.09.30 수요일',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            overflow: TextOverflow.ellipsis, // 텍스트 오버플로우 방지
-                          ),
-                        ),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Icon(Icons.insert_emoticon, color: Color(0xFFE9D899)), // 감정 아이콘 색상
-                            SizedBox(width: 4), // 아이콘 간 간격 줄이기
-                            Icon(Icons.edit, color: Colors.black), // 수정 아이콘
-                            SizedBox(width: 4), // 아이콘 간 간격 줄이기
-                            Icon(Icons.delete, color: Colors.black), // 삭제 아이콘
+                            Flexible(
+                              child: Text(
+                                '2024.09.30 수요일',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.insert_emoticon, color: Color(0xFFE9D899)),
+                                SizedBox(width: 4),
+                                Icon(Icons.edit, color: Colors.black),
+                                SizedBox(width: 4),
+                                Icon(Icons.delete, color: Colors.black),
+                              ],
+                            ),
                           ],
+                        ),
+                        SizedBox(height: 16.0),
+                        Expanded(
+                          child: Text(
+                            '일기 요약본이 여기에 표시된다. 오늘은 기획이 바뀌어서 바쁜 하루였다. 할게 너무 많아서 슬펐다. 주말도 해야되나 걱정이 되었다. 열심히 해서 주말엔 모두가 쉴 수 있으면 좋겠다.',
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 16.0),
-                    Expanded(
-                      child: Text(
-                        '일기 요약본이 여기에 표시된다. 오늘은 기획이 바뀌어서 바쁜 하루였다. 할게 너무 많아서 슬펐다. 주말도 해야되나 걱정이 되었다. 열심히 해서 주말엔 모두가 쉴 수 있으면 좋겠다.',
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 30,
+            right: 25,
+            child: GestureDetector(
+              onTap: _toggleGiftMenu,
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: Color(0xFF8D83FF),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      Colors.white, // 이미지 색상을 하얀색으로 변경
+                      BlendMode.srcIn,
                     ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          if (_isGiftMenuVisible) ...[
-                            IconButton(
-                              icon: Icon(Icons.notes, color: Colors.black),
-                              onPressed: () {
-                                // 알림 노트 동작 정의
-                              },
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.share, color: Colors.black),
-                              onPressed: () {
-                                // 공유 동작 정의
-                              },
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.print, color: Colors.black),
-                              onPressed: () {
-                                // 출력 동작 정의
-                              },
-                            ),
-                          ],
-                          IconButton(
-                            icon: Icon(Icons.card_giftcard, color: Colors.black), // 선물 아이콘
-                            onPressed: _toggleGiftMenu,
-                          ),
-                        ],
-                      ),
+                    child: Image.asset(
+                      'assets/gift_icon.png',
+                      width: 30,
+                      height: 30,
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
