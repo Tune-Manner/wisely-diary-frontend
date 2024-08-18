373af2747c49a04905dec471bd0263fa6bcc245a
djfksdl <djfksdl@naver.com>
2024-08-18 22:39:38 +0900

    feat : 감정통계 페이지 메인 감정 이모지와 월,이름 호출 성공

--- lib/statistics/monthly_emotion_screens.dart
+++ lib/statistics/monthly_emotion_screens.dart
@@ -1,7 +1,9 @@
-import 'dart:convert'; // JSON 디코딩을 위해 사용
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:http/http.dart' as http;
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 class MonthlyEmotionScreen extends StatelessWidget {
   @override
@@ -37,94 +39,128 @@ class MonthlyEmotionScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(8.0),
             ),
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // 감정 차트 제목
-                Stack(
-                  children: [
-                    // 형광펜 효과
-                    Positioned(
-                      top: 16,
-                      left: 0,
-                      child: Container(
-                        width: 150,
-                        height: 20,
-                        color: Colors.yellow.withOpacity(0.5),
-                      ),
-                    ),
-                    // 텍스트
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "이번 달 00님 감정",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                // 커스텀 반원 차트
-                SizedBox(
-                  height: 200,
-                  child: CustomSemiDonutChart(),
-                ),
-                // 주된 감정 이미지와 텍스트
-                Column(
-                  children: [
-                    Image.asset(
-                      'assets/emotions/anger.png',
-                      height: 200,
-                    ),
-                    RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        style: TextStyle(fontSize: 18, color: Colors.black),
+            child: FutureBuilder<Map<String, dynamic>>(
+              future: fetchEmotionData(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (snapshot.hasData) {
+                  Map<String, dynamic> data = snapshot.data!;
+                  Map<String, dynamic> thisMonthEmotions = data['thisMonthEmotions'];
+                  Map<String, dynamic> yearlyEmotionsRaw = data['yearlyEmotions'];
+
+                  // 사용자 이름 가져오기
+                  String memberName = data['memberName'] ?? '사용자'; // 기본값 설정
+
+                  // 가장 높은 퍼센티지를 가진 emotion_code 찾기
+                  String maxEmotionCode = thisMonthEmotions.keys.first;
+                  double maxPercentage = thisMonthEmotions[maxEmotionCode];
+
+                  thisMonthEmotions.forEach((emotionCode, percentage) {
+                    if (percentage > maxPercentage) {
+                      maxEmotionCode = emotionCode;
+                      maxPercentage = percentage;
+                    }
+                  });
+
+                  // 감정 코드와 이모티콘 이미지 파일을 매핑
+                  final Map<String, String> emotionImageMap = {
+                    '1': 'assets/emotions/worried.png',
+                    '2': 'assets/emotions/proud.png',
+                    '3': 'assets/emotions/greatful.png',
+                    '4': 'assets/emotions/injustice.png',
+                    '5': 'assets/emotions/anger.png',
+                    '6': 'assets/emotions/sad.png',
+                    '7': 'assets/emotions/joy.png',
+                    '8': 'assets/emotions/lovely.png',
+                    '9': 'assets/emotions/relax.png',
+                    '10': 'assets/emotions/embarrassed.png'
+                  };
+
+                  String emotionName = getEmotionNameByCode(maxEmotionCode);
+
+                  // 키와 값을 변환해서 Map<int, int>로 만듦
+                  Map<int, int> yearlyEmotions = yearlyEmotionsRaw.map((key, value) {
+                    return MapEntry(int.parse(key), value as int);
+                  });
+
+                  // 현재 월 구하기
+                  String currentMonth = "${DateTime.now().month}월";
+
+                  return Column(
+                    children: [
+                      // 감정 차트 제목
+                      Stack(
                         children: [
-                          TextSpan(text: '7월 00님의 메인 감정은\n'),
-                          TextSpan(
-                            text: '분노(47%)',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.red,
+                          Positioned(
+                            top: 16,
+                            left: 0,
+                            child: Container(
+                              width: 150,
+                              height: 20,
+                              color: Colors.yellow.withOpacity(0.5),
                             ),
                           ),
-                          TextSpan(text: '입니다',
-                              style: TextStyle(fontSize: 18)),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "이번 달 $memberName님 감정",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                // 감정 달력
-                FutureBuilder<Map<String, dynamic>>(
-                  future: fetchEmotionData(), // 비동기 데이터 요청
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator()); // 데이터 로딩 중
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}')); // 오류 발생 시
-                    } else if (snapshot.hasData) {
-                      // 먼저 Map<String, dynamic>으로 받고 변환 작업을 수행
-                      Map<String, dynamic> rawYearlyEmotions = snapshot.data!['yearlyEmotions'];
-
-                      // 키와 값을 변환해서 Map<int, int>로 만듦
-                      Map<int, int> yearlyEmotions = rawYearlyEmotions.map((key, value) {
-                        return MapEntry(int.parse(key), value as int);
-                      });
-
-                      return _buildEmotionTable(yearlyEmotions);
-                    } else {
-                      return Center(child: Text('No Data Available'));
-                    }
-                  },
-                ),
-              ],
+                      // 커스텀 반원 차트
+                      SizedBox(
+                        height: 200,
+                        child: CustomSemiDonutChart(
+                          emotions: thisMonthEmotions,
+                        ),
+                      ),
+                      // 주된 감정 이미지와 텍스트
+                      Column(
+                        children: [
+                          Image.asset(
+                            // 해당 감정 코드에 맞는 이미지 경로 반환
+                            emotionImageMap[maxEmotionCode] ?? 'assets/emotions/default.png',
+                            height: 200,
+                          ),
+                          RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                              children: [
+                                TextSpan(text: '$currentMonth $memberName님의 메인 감정은\n'),
+                                TextSpan(
+                                  text: '$emotionName(${maxPercentage.toStringAsFixed(0)}%)',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                TextSpan(
+                                    text: '입니다', style: TextStyle(fontSize: 18)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16),
+                      // 감정 달력
+                      _buildEmotionTable(yearlyEmotions),
+                    ],
+                  );
+                } else {
+                  return Center(child: Text('No Data Available'));
+                }
+              },
             ),
           ),
         ),
@@ -134,25 +170,48 @@ class MonthlyEmotionScreen extends StatelessWidget {
 
   // HTTP 요청을 보내서 감정 데이터를 받아오는 함수
   Future<Map<String, dynamic>> fetchEmotionData() async {
+    final supabase = Supabase.instance.client;
+    final user = supabase.auth.currentUser;
+
+    if (user == null) {
+      throw Exception('User not logged in');
+    }
+
+    final memberId = user.id;
     final url = Uri.parse('http://192.168.123.103:8080/api/statistics/inquire');
     final response = await http.post(
       url,
       headers: {"Content-Type": "application/json"},
       body: json.encode({
-        "memberId": "d199580f-bf69-4559-93b7-fe8a8ff018cf", // 예시로 memberId와 date를 보냅니다.
+        "memberId": memberId,
         "date": "2024-08-18"
       }),
     );
 
     if (response.statusCode == 200) {
-      print("성공");
       print(response.body);
-      return json.decode(response.body); // 서버로부터 받은 JSON 데이터를 디코딩
+      final decodedResponse = utf8.decode(response.bodyBytes);
+      return json.decode(decodedResponse);
     } else {
-      print("실패");
-      print(response.body);
       throw Exception('Failed to load emotion data');
     }
+  }
+
+  // 감정 이름을 반환하는 함수 (예: '분노', '슬픔' 등)
+  String getEmotionNameByCode(String code) {
+    final emotionNames = {
+      '1': '걱정',
+      '2': '자랑',
+      '3': '감사',
+      '4': '부당함',
+      '5': '분노',
+      '6': '슬픔',
+      '7': '기쁨',
+      '8': '사랑스러움',
+      '9': '안정',
+      '10': '당황'
+    };
+    return emotionNames[code] ?? '알 수 없음';
   }
 
   // 감정 달력을 생성하는 함수
@@ -235,16 +294,24 @@ class MonthlyEmotionScreen extends StatelessWidget {
 }
 
 class CustomSemiDonutChart extends StatelessWidget {
+  final Map<String, dynamic> emotions;
+
+  CustomSemiDonutChart({required this.emotions});
+
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
       size: Size(300, 150), // 차트의 크기 조정
-      painter: SemiDonutPainter(),
+      painter: SemiDonutPainter(emotions: emotions),
     );
   }
 }
 
 class SemiDonutPainter extends CustomPainter {
+  final Map<String, dynamic> emotions;
+
+  SemiDonutPainter({required this.emotions});
+
   @override
   void paint(Canvas canvas, Size size) {
     final centerX = size.width / 2;
@@ -255,18 +322,23 @@ class SemiDonutPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 100.0;
 
-    final List<EmotionData> emotions = [
-      EmotionData('분노', 0.47, Colors.red),
-      EmotionData('슬픔', 0.30, Colors.blue),
-      EmotionData('역움', 0.10, Colors.green),
-      EmotionData('기쁨', 0.08, Colors.yellow),
-      EmotionData('설렘', 0.05, Colors.purple),
-    ];
+    final emotionColors = {
+      '1': Colors.red,
+      '2': Colors.blue,
+      '3': Colors.green,
+      '4': Colors.orange,
+      '5': Colors.yellow,
+      '6': Colors.purple,
+      '7': Colors.cyan,
+      '8': Colors.pink,
+      '9': Colors.teal,
+      '10': Colors.amber,
+    };
 
     double startAngle = pi;
-    for (var emotion in emotions) {
-      final sweepAngle = emotion.percentage * pi;
-      paint.color = emotion.color;
+    emotions.forEach((emotionCode, percentage) {
+      final sweepAngle = (percentage as double) * pi / 100;
+      paint.color = emotionColors[emotionCode] ?? Colors.grey;
 
       canvas.drawArc(
         Rect.fromCircle(center: Offset(centerX, centerY), radius: radius),
@@ -276,27 +348,12 @@ class SemiDonutPainter extends CustomPainter {
         paint,
       );
 
-      // 텍스트 추가
-      final textAngle = startAngle + (sweepAngle / 2);
-      final textX = centerX + radius * cos(textAngle);
-      final textY = centerY + radius * sin(textAngle);
-
-      final textPainter = TextPainter(
-        text: TextSpan(
-          text: emotion.name,
-          style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-        ),
-        textDirection: TextDirection.ltr,
-      );
-      textPainter.layout();
-      textPainter.paint(canvas, Offset(textX - textPainter.width / 2, textY - textPainter.height / 2));
-
       startAngle += sweepAngle;
-    }
+    });
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
 
 class EmotionData {
